539072bf46bd5cdbc731ff8be4cf57abb0948be1
Tal Yanai <tyanai@global.cadence.com>
2023-06-10 12:24:57 +0300

    Update parallel example

--- docs/examples/ongoing_report.js
+++ docs/examples/ongoing_report.js
@@ -1,12 +1,11 @@
+REGRESSION_IS_DONE = false
+
 pipeline { 
     agent any 
     options {
         skipStagesAfterUnstable()
     }
-    environment {
-		//A global variable to indicate when the regression is over.
-        REGRESSION_IS_DONE = false
-    }
+   
     stages {
         
         stage('Build') {
@@ -16,11 +15,11 @@ pipeline {
                     steps { 
                         script {
                             try {
-                                vmanagerLaunch userFarmType: null, famMode: null, masterWorkspaceLocation: null, defineVariableType: null, defineVariableText: null, advConfig: false, archivePassword: '', archiveUser: '', attrValues: false, attrValuesFile: '', authRequired: true, connTimeout: 1, credentialInputFile: '', defineVarible: false, defineVaribleFile: '', deleteAlsoSessionDirectory: false, deleteCredentialInputFile: false, deleteInputFile: false, deleteSessionInputFile: false, doneResolver: 'ignore', dynamicUserId: false, envSourceInputFile: '', envSourceInputFileType: 'BSH', envVarible: false, envVaribleFile: '', executionScript: '', executionShellLocation: '', executionType: 'launcher', executionVsifFile: '', extraAttributesForFailures: false, failJobIfAllRunFailed: false, failJobUnlessAllRunPassed: false, failedResolver: 'fail', famModeLocation: '', generateJUnitXML: false, genericCredentialForSessionDelete: false, inaccessibleResolver: 'fail', markBuildAsFailedIfAllRunFailed: false, markBuildAsPassedIfAllRunPassed: false, noAppendSeed: false, pauseSessionOnBuildInterruption: false, pipelineNodes: false, readTimeout: 30, sessionsInputFile: '', staticAttributeList: '', stepSessionTimeout: 30, stoppedResolver: 'fail', suspendedResolver: 'ignore', useUserOnFarm: false, userPrivateSSHKey: false, vAPIPassword: 'letmein', vAPIUrl: 'https://vlnx488:50500/vmgr/vapi', vAPIUser: 'root', vMGRBuildArchive: false, vSIFInputFile: '', vSIFName: '/home/tyanai/vsif/test.vsif', vsifType: 'static', waitTillSessionEnds: true
+                                vmanagerLaunch userFarmType: null, famMode: null, masterWorkspaceLocation: null, defineVariableType: null, defineVariableText: null, advConfig: false, archivePassword: '', archiveUser: '', attrValues: false, attrValuesFile: '', authRequired: true, connTimeout: 1, credentialInputFile: '', defineVarible: false, defineVaribleFile: '', deleteAlsoSessionDirectory: false, deleteCredentialInputFile: false, deleteInputFile: false, deleteSessionInputFile: false, doneResolver: 'ignore', dynamicUserId: false, envSourceInputFile: '', envSourceInputFileType: 'BSH', envVarible: false, envVaribleFile: '', executionScript: '', executionShellLocation: '', executionType: 'launcher', executionVsifFile: '', extraAttributesForFailures: false, failJobIfAllRunFailed: false, failJobUnlessAllRunPassed: false, failedResolver: 'fail', famModeLocation: '', generateJUnitXML: false, genericCredentialForSessionDelete: false, inaccessibleResolver: 'fail', markBuildAsFailedIfAllRunFailed: false, markBuildAsPassedIfAllRunPassed: false, noAppendSeed: false, pauseSessionOnBuildInterruption: false, pipelineNodes: false, readTimeout: 30, sessionsInputFile: '', staticAttributeList: '', stepSessionTimeout: 30, stoppedResolver: 'ignore', suspendedResolver: 'ignore', useUserOnFarm: false, userPrivateSSHKey: false, vAPIPassword: 'letmein', vAPIUrl: 'https://vlnx488:50500/vmgr/vapi', vAPIUser: 'root', vMGRBuildArchive: false, vSIFInputFile: '', vSIFName: '/home/tyanai/vsif/test.vsif', vsifType: 'static', waitTillSessionEnds: true
                             } finally {
 								//Must reside within try/catch block as the build itself might fail
                                 echo "Marking the regression as completed for the report job (the other parallel branch) to also stop running"
-                                REGRESSION_IS_DONE = true
+                                REGRESSION_IS_DONE = 'true'
                             }
                         }
                     }
@@ -29,11 +28,12 @@ pipeline {
                     steps {
                         //No point in trying to get the report before 2 minutes passed.  It takes at least 1 minute for the regression
 						//to create the session_status.properties for the build which is also an indication that the report can be generated
-                        sleep(time:120,unit:"SECONDS")
+                        sleep(time:150,unit:"SECONDS")
                         
                         
                         //The below is to make sure we don't try to bring the report before the file session_status.properties was created
 						//However, in case 2 minutes passed since the regression started, most likely something is wrong so timeout here is 2 minutes.
+						
                         timeout(time:2,unit:'MINUTES') {
                             script {
                                 waitUntil {
@@ -56,14 +56,23 @@ pipeline {
                         
                         script {
 						
-                            def timeToWait = 10
+                            def timeToWait = 60  //Waiting time in second between each report
+                            echo "REGRESSION_IS_DONE: " + REGRESSION_IS_DONE
                             while (!REGRESSION_IS_DONE) {
-                                vmanagerPostBuildActions metricsInputType: null, vPlanInputType: null, emailType: null, advConfig: false, advancedFunctions: true, authRequired: true, connTimeout: 1, ctxAdvanceInput: '', ctxInput: false, deleteEmailInputFile: false, deleteReportSyntaxInputFile: false, dynamicUserId: false, emailInputFile: '', emailList: '', freeVAPISyntax: '', ignoreSSLError: false, metricsAdvanceInput: '', metricsDepth: 6, metricsReport: false, metricsViewName: 'All_Metrics', readTimeout: 30, retrieveSummaryReport: true, runReport: true, sendEmail: false, summaryMode: 'full', summaryType: 'wizard', testsDepth: 6, testsViewName: 'Test_Hierarchy', vAPIPassword: 'letmein', vAPIUrl: 'https://vlnx488:50500/vmgr/vapi', vAPIUser: 'root', vManagerVersion: 'stream', vPlanAdvanceInput: '', vPlanDepth: 6, vPlanReport: false, vPlanxFileName: '', vplanViewName: 'All_Vplan'
+                              
+                                try {
+                                    vmanagerPostBuildActions metricsInputType: null, vPlanInputType: null, emailType: null, advConfig: false, advancedFunctions: true, authRequired: true, connTimeout: 1, ctxAdvanceInput: '', ctxInput: false, deleteEmailInputFile: false, deleteReportSyntaxInputFile: false, dynamicUserId: false, emailInputFile: '', emailList: '', freeVAPISyntax: '', ignoreSSLError: false, metricsAdvanceInput: '', metricsDepth: 6, metricsReport: false, metricsViewName: 'All_Metrics', readTimeout: 30, retrieveSummaryReport: true, runReport: true, sendEmail: false, summaryMode: 'full', summaryType: 'wizard', testsDepth: 6, testsViewName: 'Test_Hierarchy', vAPIPassword: 'letmein', vAPIUrl: 'https://vlnx488:50500/vmgr/vapi', vAPIUser: 'root', vManagerVersion: 'stream', vPlanAdvanceInput: '', vPlanDepth: 6, vPlanReport: false, vPlanxFileName: '', vplanViewName: 'All_Vplan'
+								} catch (err) {
+								     //Must reside within try/catch block as the build itself might fail
+                                    echo err.getMessage()
+								    echo "Attempt to bring the report failed. Marking for Exit"
+                                    REGRESSION_IS_DONE = true
+                                }
 								//The below line is optional - it adds a link to the summary report right below the history table on the left.
 								currentBuild.description = "Click <a href='./${env.BUILD_NUMBER}/vManagerSummaryReport'>Regression Progress Report</a>"
 								
 								//Wait X amount of seconds before trying to fetch yet an additonal report
-								echo "Waiting ${timeToWait} seconds before updating/fetching the summary report..."
+								echo "Waiting ${timeToWait} seconds before updating/fetching the summary report again..."
                                 sleep(time:timeToWait,unit:"SECONDS")
                             }
                         }
@@ -79,11 +88,11 @@ pipeline {
 			//In any case, bring the report one last time at the end of the execution
             steps {
                 vmanagerPostBuildActions metricsInputType: null, vPlanInputType: null, emailType: null, advConfig: false, advancedFunctions: true, authRequired: true, connTimeout: 1, ctxAdvanceInput: '', ctxInput: false, deleteEmailInputFile: false, deleteReportSyntaxInputFile: false, dynamicUserId: false, emailInputFile: '', emailList: '', freeVAPISyntax: '', ignoreSSLError: false, metricsAdvanceInput: '', metricsDepth: 6, metricsReport: false, metricsViewName: 'All_Metrics', readTimeout: 30, retrieveSummaryReport: true, runReport: true, sendEmail: false, summaryMode: 'full', summaryType: 'wizard', testsDepth: 6, testsViewName: 'Test_Hierarchy', vAPIPassword: 'letmein', vAPIUrl: 'https://vlnx488:50500/vmgr/vapi', vAPIUser: 'root', vManagerVersion: 'stream', vPlanAdvanceInput: '', vPlanDepth: 6, vPlanReport: false, vPlanxFileName: '', vplanViewName: 'All_Vplan'
-				
-				//The below line is optional - it adds a link to the summary report right below the history table on the left.
-				script{
-					currentBuild.description = "Click <a href='./${env.BUILD_NUMBER}/vManagerSummaryReport'>Regression Progress Report</a>"
-				}
+                
+                //The below line is optional - it adds a link to the summary report right below the history table on the left.
+                script {
+                    currentBuild.description = "Click <a href='./${env.BUILD_NUMBER}/vManagerSummaryReport'>Regression Progress Report</a>"
+                }
             }
         }
     }
